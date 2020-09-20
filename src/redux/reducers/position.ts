78d7fe7c46d05491ec135bcbf19d0f33aa6ee577
kinothUI <crypt0@runbox.at>
2020-09-20 15:44:01 +0200

    bugfix
    redux/reducers/position.ts: failure description documented in code

--- src/redux/reducers/position.ts
+++ src/redux/reducers/position.ts
@@ -21,6 +21,12 @@ function positionReducer(
           const update = action.position[0];
           // positions are unique by symbol
           const index = state.content.findIndex((item) => item.symbol === update.symbol);
+          // when posting a new order for an symbol where no position was opened yet,
+          // bitmex sends an initial position message what causes the app to crash.
+          // therefore testing on -1 not to invoke updateSingleEntity() Function.
+          // There might be a more elegant way to handle this behaviour but this works for now :-)
+          if (index === -1) return _.assign({}, { ...state, isLoading: false });
+
           const positionToUpdate = state.content[index];
           const content = updateSingleEntity(positionToUpdate, update, index, state.content);
 
